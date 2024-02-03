264f4021ef25e3ec8f7686239b237b084c09daa6
shiraz-9898 <shiraazbeg9898@gmail.com>
2024-02-03 11:56:22 +0530

    Add project files.

---
+++ AbstracttionExaple/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstracttionExaple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AbsChild obj = new AbsChild();
+
+            obj.Add(10, 2);
+            obj.Sub(10, 2);
+            obj.Mul(10, 2);
+            obj.Div(10, 2);
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public abstract class AbsParent
+    {
+        public void Add(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+
+        public void Sub(int num1, int num2)
+        {
+            Console.WriteLine(num1 - num2);
+        }
+
+        public abstract void Mul(int num1, int num2);
+        public abstract void Div(int num1, int num2);
+    }
+
+    public class AbsChild : AbsParent
+    {
+        public override void Mul(int num1, int num2)
+        {
+            Console.WriteLine(num1 * num2);
+        }
+
+        public override void Div(int num1, int num2)
+        {
+            Console.WriteLine(num1 / num2);
+        }
+    }
+}
